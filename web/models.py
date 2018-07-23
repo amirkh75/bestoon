af84947246f2e228aed39c44b8bbf1f807953649
amirkh <amirp.1375@gmail.com>
2018-07-23 18:06:38 -0400

    writ web on bestoon settingp.y installed_ap

--- web/models.py
+++ web/models.py
@@ -1,11 +1,15 @@
-# -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
 from django.db import models
+
 from django.contrib.auth.models import User
 
-# Create your models here.
-class Expense(models.Model):
+
+class Expensee(models.Model):
     title = models.CharField(max_length=255)
     date = models.DateTimeField(auto_now_add=True)
     amount = models.BigIntegerField(default=0)
     user = models.ForeignKey(User)
+    amount1 = models.BigIntegerField(default=0)  
+    amount3 = models.BigIntegerField(default=0)
+
